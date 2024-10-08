38b8a5d0184852c96bd3433236b782a714ab6c9a
Jorgegiral <jorgegiraldo758@gmail.com>
2024-10-08 13:06:55 +0200

    tercera_subida

--- PruebasURP/Assets/RaB_Base/Scripts/PlayerInteraction.cs
+++ PruebasURP/Assets/RaB_Base/Scripts/PlayerInteraction.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
+    [Header("Point System Parameters")]
     // Variables para definir los puntos del jugador
     public int currentPoints;
     public int winPoints;
 
+    [Header("Respawn Parameters")]
+    public Transform respawnPoint;
+    public float respawnFallLimit;
+
     private void Update()
     {
         if (currentPoints < 0) { currentPoints = 0; }
+        if (transform.position.y <= respawnFallLimit) { Respawn(); }
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -26,5 +32,14 @@ public class PlayerInteraction : MonoBehaviour
             other.gameObject.SetActive(false);
             // Destroy(other.gameObject);
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle")) { Respawn(); }
+    }
+    void Respawn()
+    {
+        transform.position = respawnPoint.position;
     }
 }
